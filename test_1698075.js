1db603593e1b4fed753c2983dd5005f0f4bd1b0b
PioneerLabs <filippo@pioneerlabs.io>
2019-09-10 17:38:41 +0100

    Create test_1698075.js

---
+++ test_1698075.js
@@ -0,0 +1,17 @@
+function main() {
+   var line = "";
+   while(line !== null){
+    line = readline();
+
+    if(line !== null ){
+        print(caesarCipher(line))
+        
+    }
+   }
+}
+
+function caesarCipher(str, key = 3) {
+   return str
+      
+       .replace(/[A-Z]/g, char => String.fromCharCode((char.charCodeAt(0) - 65 + key ) % 26 + 65));
+}
