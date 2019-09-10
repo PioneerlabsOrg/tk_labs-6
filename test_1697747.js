fd4a9ff7be312c24531c41bfa7970c455c8e8f06
PioneerLabs <filippo@pioneerlabs.io>
2019-09-10 17:33:10 +0100

    Create test_1697747.js

---
+++ test_1697747.js
@@ -0,0 +1,21 @@
+function caesarCipher(str, key = 3) {
+   return str
+       .toUpperCase()
+       .replace(/[A-Z]/g, char => String.fromCharCode((char.charCodeAt(0) - 65 + key ) % 26 + 65));
+       
+}
+
+
+function main() {
+   var line = "";
+   while(line !== null){
+    line = readline();
+
+    if(line !== null ){
+        print(caesarCipher(line))
+        
+    }
+}
+
+
+}
