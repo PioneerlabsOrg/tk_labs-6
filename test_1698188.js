c25928540bd467650a6299ed3fe1c5d2c1e96dde
PioneerLabs <filippo@pioneerlabs.io>
2019-09-10 17:31:12 +0100

    Create test_1698188.js

---
+++ test_1698188.js
@@ -0,0 +1,44 @@
+while((line = readline()) !== null){
+	
+	doEncryption(line.split("\n"))
+
+	
+}
+
+function doEncryption(input){
+	var result = ""
+	var string = input.toString()
+	for(var i=0; i<string.length; i++){
+		var char = string.charAt(i);
+		if(char !== undefined){
+			result += encrypt(char)
+		}
+	}
+	print(result)
+}
+
+function encrypt(input){
+  var cipherText = input;
+  var cipherOffset = 3;
+  var alphabet = "abcdefghijklmnopqrstuvwxyz";
+  var fullAlphabet = alphabet + alphabet + alphabet;
+  cipherOffset = (cipherOffset % alphabet.length);
+  var cipherFinish = '';
+
+  for(i=0; i<cipherText.length; i++){
+     var letter = cipherText[i];
+     var upper = (letter == letter.toUpperCase());
+     letter = letter.toLowerCase();
+     var index = alphabet.indexOf(letter);
+     if(index == -1){
+       cipherFinish += letter;
+     } else {
+       index = ((index + cipherOffset) + alphabet.length);
+       var nextLetter = fullAlphabet[index];
+       if(upper) nextLetter = nextLetter.toUpperCase();
+       cipherFinish += nextLetter;
+     }
+  }
+  
+  return cipherFinish;
+}
